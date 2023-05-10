a269ae64f1c8cb559859a3a99f9c8f4cb88d9142
Hector <hectorbennett@outlook.com>
2023-05-10 12:27:25 +0100

    Add webview example

--- using_webview/src/lib.rs
+++ using_webview/src/lib.rs
@@ -1,76 +1,85 @@
+// Forked and modified from: https://github.com/robbert-vdh/nih-plug/tree/master/plugins/examples/gain
 use nih_plug::prelude::*;
+use nih_plug_webview::*;
+use serde::Deserialize;
+use serde_json::json;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
-mod drive;
+struct Gain {
+    params: Arc<GainParams>,
+}
 
-struct UsingWebview {
-    params: Arc<UsingWebviewParams>,
-
-    sample_rate: f32,
+#[derive(Deserialize)]
+#[serde(tag = "type")]
+enum Action {
+    Init,
+    SetSize { width: u32, height: u32 },
+    SetGain { value: f32 },
 }
 
 #[derive(Params)]
-struct UsingWebviewParams {
+struct GainParams {
     #[id = "gain"]
-    output_gain: FloatParam,
-
-    #[id = "drive"]
-    drive: FloatParam,
+    pub gain: FloatParam,
+    gain_value_changed: Arc<AtomicBool>,
 }
 
-impl Default for UsingWebview {
+impl Default for Gain {
     fn default() -> Self {
         Self {
-            params: Arc::new(UsingWebviewParams::default()),
-            sample_rate: 1.0,
+            params: Arc::new(GainParams::default()),
         }
     }
 }
 
-impl Default for UsingWebviewParams {
+impl Default for GainParams {
     fn default() -> Self {
+        let gain_value_changed = Arc::new(AtomicBool::new(false));
+
+        let v = gain_value_changed.clone();
+        let param_callback = Arc::new(move |_: f32| {
+            v.store(true, Ordering::Relaxed);
+        });
+
         Self {
-            output_gain: FloatParam::new(
+            gain: FloatParam::new(
                 "Gain",
                 util::db_to_gain(0.0),
-                FloatRange::Linear {
-                    min: util::db_to_gain(-24.0),
-                    max: util::db_to_gain(0.0),
+                FloatRange::Skewed {
+                    min: util::db_to_gain(-30.0),
+                    max: util::db_to_gain(30.0),
+                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
                 },
             )
-            .with_smoother(SmoothingStyle::Logarithmic(10.0))
+            .with_smoother(SmoothingStyle::Logarithmic(50.0))
             .with_unit(" dB")
             .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
-            .with_string_to_value(formatters::s2v_f32_gain_to_db()),
-
-            drive: FloatParam::new(
-                "Drive",
-                200.0,
-                FloatRange::Skewed {
-                    min: 100.0,
-                    max: 10_000.0,
-                    factor: FloatRange::skew_factor(-2.0),
-                },
-            )
-            .with_smoother(SmoothingStyle::Linear(30.0))
-            .with_unit("%"),
+            .with_string_to_value(formatters::s2v_f32_gain_to_db())
+            .with_callback(param_callback.clone()),
+            gain_value_changed,
         }
     }
 }
 
-impl Plugin for UsingWebview {
-    const NAME: &'static str = "UsingWebview";
-    const VENDOR: &'static str = "Hector";
-    const URL: &'static str = env!("CARGO_PKG_HOMEPAGE");
-    const EMAIL: &'static str = "contact@hectorbennett.com";
+impl Plugin for Gain {
+    type BackgroundTask = ();
+    type SysExMessage = ();
 
-    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+    const NAME: &'static str = "Gain";
+    const VENDOR: &'static str = "Moist Plugins GmbH";
+    const URL: &'static str = "https://youtu.be/dQw4w9WgXcQ";
+    const EMAIL: &'static str = "info@example.com";
+
+    const VERSION: &'static str = "0.0.1";
 
     const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[
         AudioIOLayout {
             main_input_channels: NonZeroU32::new(2),
             main_output_channels: NonZeroU32::new(2),
-            ..AudioIOLayout::const_default()
+            aux_input_ports: &[],
+            aux_output_ports: &[],
+            names: PortNames::const_default(),
         },
         AudioIOLayout {
             main_input_channels: NonZeroU32::new(1),
@@ -79,30 +88,12 @@ impl Plugin for UsingWebview {
         },
     ];
 
-    type SysExMessage = ();
-    type BackgroundTask = ();
+    const MIDI_INPUT: MidiConfig = MidiConfig::None;
+    const SAMPLE_ACCURATE_AUTOMATION: bool = true;
 
     fn params(&self) -> Arc<dyn Params> {
         self.params.clone()
     }
-
-    fn initialize(
-        &mut self,
-        audio_io_layout: &AudioIOLayout,
-        buffer_config: &BufferConfig,
-        _context: &mut impl InitContext<Self>,
-    ) -> bool {
-        self.sample_rate = buffer_config.sample_rate;
-
-        let num_output_channels = audio_io_layout
-            .main_output_channels
-            .expect("Plugin does not have a main output")
-            .get() as usize;
-
-        true
-    }
-
-    fn reset(&mut self) {}
 
     fn process(
         &mut self,
@@ -110,38 +101,88 @@ impl Plugin for UsingWebview {
         _aux: &mut AuxiliaryBuffers,
         _context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
-        for mut channel_samples in buffer.iter_samples() {
-            let output_gain = self.params.output_gain.smoothed.next();
-            let drive = self.params.drive.smoothed.next();
+        for channel_samples in buffer.iter_samples() {
+            let gain = self.params.gain.smoothed.next();
 
-            for sample in channel_samples.iter_mut() {
-                *sample = drive::drive(sample.clone().into(), drive) * output_gain;
+            for sample in channel_samples {
+                *sample *= gain;
             }
         }
 
         ProcessStatus::Normal
     }
+
+    fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
+        let params = self.params.clone();
+        let gain_value_changed = self.params.gain_value_changed.clone();
+        let editor = WebViewEditor::new(HTMLSource::String(include_str!("gui.html")), (200, 200))
+            .with_background_color((150, 150, 150, 255))
+            .with_developer_mode(true)
+            .with_event_loop(move |ctx, setter| {
+                while let Some(event) = ctx.next_event() {
+                    match event {
+                        WebviewEvent::JSON(value) => {
+                            if let Ok(action) = serde_json::from_value(value) {
+                                match action {
+                                    Action::SetGain { value } => {
+                                        setter.begin_set_parameter(&params.gain);
+                                        setter.set_parameter_normalized(&params.gain, value);
+                                        setter.end_set_parameter(&params.gain);
+                                    }
+                                    Action::SetSize { width, height } => {
+                                        ctx.resize(width, height);
+                                    }
+                                    Action::Init => {
+                                        let _ = ctx.send_json(json!({
+                                            "type": "set_size",
+                                            "width": ctx.width.load(Ordering::Relaxed),
+                                            "height": ctx.height.load(Ordering::Relaxed)
+                                        }));
+                                    }
+                                }
+                            } else {
+                                panic!("Invalid action received from web UI.")
+                            }
+                        }
+                        WebviewEvent::FileDropped(path) => println!("File dropped: {:?}", path),
+                        _ => {}
+                    }
+                }
+
+                if gain_value_changed.swap(false, Ordering::Relaxed) {
+                    let _ = ctx.send_json(json!({
+                        "type": "param_change",
+                        "param": "gain",
+                        "value": params.gain.unmodulated_normalized_value(),
+                        "text": params.gain.to_string()
+                    }));
+                }
+            });
+
+        Some(Box::new(editor))
+    }
+
+    fn deactivate(&mut self) {}
 }
 
-impl ClapPlugin for UsingWebview {
-    const CLAP_ID: &'static str = "hectorbennett.using_webview";
-    const CLAP_DESCRIPTION: Option<&'static str> = Some("UsingWebviewooo");
+impl ClapPlugin for Gain {
+    const CLAP_ID: &'static str = "com.moist-plugins-gmbh.gain";
+    const CLAP_DESCRIPTION: Option<&'static str> = Some("A smoothed gain parameter example plugin");
     const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
     const CLAP_SUPPORT_URL: Option<&'static str> = None;
     const CLAP_FEATURES: &'static [ClapFeature] = &[
         ClapFeature::AudioEffect,
         ClapFeature::Stereo,
         ClapFeature::Mono,
-        ClapFeature::Limiter,
-        ClapFeature::Distortion,
+        ClapFeature::Utility,
     ];
 }
 
-impl Vst3Plugin for UsingWebview {
-    const VST3_CLASS_ID: [u8; 16] = *b"UsingWebviewoooooooo";
+impl Vst3Plugin for Gain {
+    const VST3_CLASS_ID: [u8; 16] = *b"GainMoistestPlug";
     const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
-        &[Vst3SubCategory::Fx, Vst3SubCategory::Distortion];
+        &[Vst3SubCategory::Fx, Vst3SubCategory::Tools];
 }
 
-nih_export_clap!(UsingWebview);
-nih_export_vst3!(UsingWebview);
+nih_export_clap!(Gain);
+nih_export_vst3!(Gain);
